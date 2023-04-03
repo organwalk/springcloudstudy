3411d9e53b1eee427b132c054a6cada508cd4b3b
haruki <harukisea0@gmail.com>
2023-04-03 21:28:48 +0800

    Eureka自定义随机负载均衡

--- borrow-service/src/main/java/com/example/config/BeanConfiguration.java
+++ borrow-service/src/main/java/com/example/config/BeanConfiguration.java
@@ -1,11 +1,14 @@
 package com.example.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@LoadBalancerClient(value = "userservice",      //指定为 userservice服务，只要是调用此服务都会使用我们指定的策略
+                    configuration = LoadBalancerConfig.class)  //指定我们刚刚定义好的配置类
 public class BeanConfiguration {
     @Bean
     @LoadBalanced
