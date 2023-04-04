56045422650f4e50742bbda4b5cb9de7c6d08c07
haruki <harukisea0@gmail.com>
2023-04-04 15:29:00 +0800

    GateWay实现网关配置及自定义过滤器

--- book-service/src/main/java/com/example/controller/BookController.java
+++ book-service/src/main/java/com/example/controller/BookController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import com.example.entity.Book;
 import com.example.service.BookService;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,9 @@ public class BookController {
     BookService service;
 
     @RequestMapping("/book/{bid}")
-    Book findBookById(@PathVariable("bid")Integer bid){
+    Book findBookById(@PathVariable("bid")Integer bid,
+                      HttpServletRequest request){
+        System.out.println(request.getHeader("Test"));
         return service.getBookById(bid);
     }
 }
